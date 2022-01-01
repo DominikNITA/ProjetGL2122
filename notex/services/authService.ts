67f341ca32217f7a271c37989d68023149d95961
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2022-01-01 01:27:15 +0100

    Dodanie authService

--- notex/services/authService.ts
+++ notex/services/authService.ts
@@ -1,17 +1,53 @@
 import { Model, Document, Types } from "mongoose"
-import { UserModel } from "../models/user"
 import { dbConnect } from "../utils/connection";
-import { IUser } from "../utils/types"
+import { IAuthData, IUser } from "../utils/types";
+import { InvalidParameterValue, NotImplementedError } from "../utils/errors";
 import * as UserService from "./userService";
+import { AuthDataModel } from "../models/authData";
+import { throwIfNullParameters, validateEmail } from "../utils/other";
+import { SHA256 } from "crypto-js";
+import { randomUUID } from "crypto";
 
-type UserReturn = (Document<any, any, IUser> & IUser & {_id: Types.ObjectId;}) | null;
 
-export async function authenticate(email: string, password: string) : Promise<UserReturn>{
+export async function authenticate(email: string, password: string): Promise<UserService.UserReturn> {
+    if (!validateEmail(email)) {
+        throw new InvalidParameterValue(email, "Email pattern does not match");
+    }
+
     await dbConnect();
     const user = await UserService.getUserByEmail(email);
+    if (user == null) {
+        return null;
+    }
+    const authData = await getAuthDataForUser(user);
+    if(authData?.passwordHash !== SHA256(authData!.salt + password).toString()){
+        return null;
+    }
+
     return user;
 }
 
-export async function createUser(user: IUser, password: string) {
-    
+export async function getAuthDataForUser(user: UserService.UserReturn) {
+    throwIfNullParameters([user]);
+    return await AuthDataModel.findOne({ 'user': user!._id });
+}
+
+export async function createAuthUser(user: IUser, password: string) {
+    throwIfNullParameters([user,password])
+    // if (await UserService.getUserByEmail(user.email) !== null) {
+    //     throw new Error();
+    // }
+    const salt = randomUUID();
+    const hashedPassword = SHA256(salt + password);
+
+    const newUser = await UserService.addNewUser(user);
+    console.log("newUser", newUser?.id)
+    const newAuthData = await addNewAuthData({user: newUser!._id, salt: salt, passwordHash: hashedPassword.toString()})
+    console.log(`New user created: email-${newUser?.email}, id-${newUser?.id}, authId-${newAuthData.id}`)
+}
+
+async function addNewAuthData(authData : IAuthData ){
+    const authDataToAdd = new AuthDataModel(authData);
+    await authDataToAdd.save();
+    return authDataToAdd;
 }
---
+++ notex/utils/other.ts
@@ -0,0 +1,14 @@
+import { InvalidParameterValue } from "./errors";
+
+export function validateEmail(email:string) : boolean {
+    const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return regexp.test(email);
+}
+
+export function throwIfNullParameters(parameters: any[]){
+    parameters.forEach(param => {
+        if(param == null){
+            throw new InvalidParameterValue(param, "Param cannot be null!");
+        }
+    });
+}
