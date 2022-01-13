3734f6e0f166c6451a2d9454dd65af4d878fa474
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2022-01-13 21:13:04 +0100

    Bypass

--- client/src/utility/RequireAuth.tsx
+++ client/src/utility/RequireAuth.tsx
@@ -5,7 +5,7 @@ export function RequireAuth({ children }: { children: JSX.Element }) {
     const auth = useAuth();
     const location = useLocation();
 
-    if (!auth || !auth.user) {
+    if ((!auth || !auth.user) && process.env.REACT_APP_BYPASS_AUTH !== 'true') {
         // Redirect them to the /login page, but save the current location they were
         // trying to go to when they were redirected. This allows us to send them
         // along to that page after they login, which is a nicer user experience
