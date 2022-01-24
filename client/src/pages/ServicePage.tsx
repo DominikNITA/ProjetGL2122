5750c8b3797545868205882493caec9fe7635bcf
bubsi-91 <66141310+bubsi-91@users.noreply.github.com>
2022-01-24 02:44:06 +0100

    Added Service Routing
    Server side:
    - Implemented serviceRouter
    Client side:
    - Created ServicePage + route
    - Implemented serviceClient

---
+++ client/src/pages/ServicePage.tsx
@@ -0,0 +1,19 @@
+import { useAuth } from '../stateProviders/authProvider';
+import {} from '../clients/serviceClient';
+
+const ServicePage = () => {
+    const auth = useAuth();
+    console.log(auth?.user);
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}> Mon service :</h1>
+            {
+                <h2 style={{ textAlign: 'center' }}>
+                    {auth?.user?.service.name}
+                </h2>
+            }
+        </div>
+    );
+};
+
+export default ServicePage;
