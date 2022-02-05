372308352a034d20d302560930fae32de9495144
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2022-02-05 22:16:41 +0100

    Add managing justificatifs on dev page

--- server/src/routes/noteRouter.ts
+++ server/src/routes/noteRouter.ts
@@ -4,7 +4,7 @@ import noteLineService from '../services/noteLineService';
 import noteService from '../services/noteService';
 import serviceService from '../services/serviceService';
 import userService, { UserReturn } from '../services/userService';
-import { ErrorResponse } from '../utility/errors';
+import { ErrorResponse, InvalidParameterValue } from '../utility/errors';
 import { AuthenticatedRequest, requireAuthToken } from '../utility/middlewares';
 import { convertStringToObjectId } from '../utility/other';
 import { INote } from '../utility/types';
@@ -182,8 +182,9 @@ const upload = multer({
             ext !== '.jpeg'
         ) {
             return callback(
-                new Error(
-                    'Seulement les fichiers .png, .jgp, .pdf, .jpeg sont acceptes'
+                new InvalidParameterValue(
+                    'justificatif',
+                    'Seulement les fichiers .png, .jpg, .jpeg, .pdf sont acceptes'
                 )
             );
         }
