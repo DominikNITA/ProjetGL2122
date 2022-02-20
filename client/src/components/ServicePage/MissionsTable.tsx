fd62d69ceed11e2879dde375ac52c9f2e07695f0
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2022-02-20 20:39:36 +0100

    Searching added, resetting doesn't work

--- client/src/components/ServicePage/MissionsTable.tsx
+++ client/src/components/ServicePage/MissionsTable.tsx
@@ -1,8 +1,12 @@
 import { blue } from '@ant-design/colors';
-import { CloseOutlined, ZoomInOutlined } from '@ant-design/icons';
-import { Space, Col, List, Button, Row, Table } from 'antd';
+import {
+    CloseOutlined,
+    SearchOutlined,
+    ZoomInOutlined,
+} from '@ant-design/icons';
+import { Space, Col, List, Button, Row, Table, Input } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
-import React from 'react';
+import React, { useState } from 'react';
 import { FraisType, MissionState, NoteLineState } from '../../enums';
 import { useAuth } from '../../stateProviders/authProvider';
 import { useSelectedMission } from '../../stateProviders/selectedMissionProvider';
@@ -28,12 +32,101 @@ const MissionsTable = ({ missions }: Props) => {
         return userId == auth?.user?.service?.leader;
     }
 
+    const [searchInput, setSearchInput] = useState<Input | null>();
+    const [searchText, setSearchText] = useState('');
+    const getColumnSearchProps = (dataIndex: string) => ({
+        filterDropdown: ({
+            setSelectedKeys,
+            selectedKeys,
+            confirm,
+            clearFilters,
+        }: any) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={(node) => {
+                        setSearchInput(node);
+                    }}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) =>
+                        setSelectedKeys(e.target.value ? [e.target.value] : [])
+                    }
+                    onPressEnter={() =>
+                        handleSearch(selectedKeys, confirm, dataIndex)
+                    }
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() =>
+                            handleSearch(selectedKeys, confirm, dataIndex)
+                        }
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => handleReset(clearFilters)}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({ closeDropdown: false });
+                            setSearchText(selectedKeys[0]);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+            <SearchOutlined
+                style={{ color: filtered ? '#1890ff' : undefined }}
+            />
+        ),
+        onFilter: (value: any, record: any) =>
+            record[dataIndex]
+                ? record[dataIndex]
+                      .toString()
+                      .toLowerCase()
+                      .includes(value.toLowerCase())
+                : '',
+        onFilterDropdownVisibleChange: (visible: boolean) => {
+            if (visible) {
+                setTimeout(() => searchInput!.select(), 100);
+            }
+        },
+    });
+
+    const handleSearch = (
+        selectedKeys: React.SetStateAction<string>[],
+        confirm: () => void,
+        dataIndex: string
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+    };
+
+    const handleReset = (clearFilters: () => void) => {
+        clearFilters();
+        setSearchText('');
+    };
+
     const allColumns: ColumnsType<IMission> = [
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
-            filtered: true,
+            ...getColumnSearchProps('name'),
         },
         {
             title: 'Description',
